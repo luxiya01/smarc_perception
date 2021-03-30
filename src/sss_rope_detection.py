0b3f8270b1a33a7ee3f417fa1c33f32a0e6105c0
Li Ling <liling@kth.se>
2021-03-30 19:05:03 +0200

    Rope detection using hough transform Debugging needed: number of detected lines goes up very quickly

--- src/sss_rope_detection.py
+++ src/sss_rope_detection.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python
 
+import cv2
+import math
 import rospy
 import numpy as np
-from smarc_msgs import Sidescan
+from smarc_msgs.msg import Sidescan
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 
 class sss_detection:
     def __init__(self, width=1000, height=2000):
         self.width = width
         self.height = height
+        self.effective_height = 0
         self.sss_img = np.zeros((self.width, self.height), dtype=np.ubyte)
         self.sss_sub = rospy.Subscriber("/sam/payload/sidescan", Sidescan,
                                         self.callback)
-        self.effective_height = 0
+        self.detection_pub = rospy.Publisher("/sam/detection/rope",
+                                             Image,
+                                             queue_size=2)
+        self.bridge = CvBridge()
 
     def callback(self, msg):
         port = np.array(bytearray(msg.port_channel), dtype=np.ubyte)
@@ -23,12 +31,45 @@ class sss_detection:
 
         self.effective_height = min(self.effective_height + 1, self.height)
 
+        self.detect_rope()
+
+    def detect_rope(self):
+        image = np.copy(self.sss_img[:self.effective_height, :])
+        image = cv2.Canny(image, 100, 150)
+        image_bgr = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+        lines = cv2.HoughLines(image,
+                               1,
+                               np.pi / 180,
+                               50,
+                               min_theta=0,
+                               max_theta=np.pi / 180 * 10)
+
+        if lines is not None:
+            for line in lines:
+                rho = line[0][0]
+                theta = line[0][1]
+                a = math.cos(theta)
+                b = math.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                pt1 = (int(x0 - 1000 * b), int(y0 + 1000 * a))
+                pt2 = (int(x0 + 1000 * b), int(y0 - 1000 * a))
+                cv2.line(image_bgr, pt1, pt2, (0, 255, 0), 3, cv2.LINE_AA)
+            print(f"Number of detected lines: {len(lines)}")
+
+        try:
+            image_bgr = self.bridge.cv2_to_imgmsg(image_bgr, "bgr8")
+            self.detection_pub.publish(image_bgr)
+
+        except CvBridgeError as e:
+            print(e)
+
 
 def main():
     rospy.init_node('sss_rope_detection', anonymous=True)
     r = rospy.Rate(5)  # 10hz
 
-    print('Node sss_rope_detection running...')
+    sss_rope_detector = sss_detection()
     try:
         rospy.spin()
     except KeyboardInterrupt:
