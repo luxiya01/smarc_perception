c206cdd9eaa707c7773c3b192d244113894c6889
Li Ling <liling@kth.se>
2021-05-12 15:50:37 +0200

    Add method to unpack channel

--- scripts/utils.py
+++ scripts/utils.py
@@ -28,7 +28,16 @@ STRUCT = {
 def unpack_struct(fileobj, struct_type):
     """Given a file object and a struct type, read the corresponding number of bytes in
     standard size, unpack the resulting buffer according to the correct format and return
-    the unpacked object"""
+    the unpacked object. Assume native byte order."""
     info = STRUCT[struct_type]
     buff = fileobj.read(info['size'])
     return struct.unpack(info['fmt'], buff)[0]
+
+
+def unpack_channel(fileobj, channel_size):
+    """Given a file object, read channel_size number of bytes from the fileobj, unpack
+    the resulting buffer as a char array with correct size. Assume native byte order.
+    Returns a List(int) of length = channel_size."""
+    fmt = f'{channel_size}B'
+    buff = fileobj.read(channel_size)
+    return list(struct.unpack(fmt, buff))
