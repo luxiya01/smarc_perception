ee4c396a5a854dd0d23754576231f73332aa7c10
Li Ling <liling@kth.se>
2021-05-17 15:07:50 +0200

    Add plot methods for DVSFile and SSSPing

--- scripts/dvs_file_reader.py
+++ scripts/dvs_file_reader.py
@@ -1,9 +1,11 @@
 from enum import Enum
 from dataclasses import dataclass
 import utils
-from typing import List, BinaryIO
+from typing import List, BinaryIO, Tuple
 import struct
 from pathlib import Path
+from matplotlib import pyplot as plt
+import numpy as np
 
 
 class Side(Enum):
@@ -21,6 +23,15 @@ class SSSPing:
     heading: float
     side: Side
     ping: List[int]
+
+    def plot(self) -> None:
+        plt.figure()
+        plt.plot(self.ping,
+                 linestyle='-',
+                 marker='o',
+                 markersize=1,
+                 linewidth=.5)
+        plt.title(f'SSS Ping, side = {self.side}')
 
 
 @dataclass
@@ -106,3 +117,76 @@ class DVSFile:
         fileobj.read(2)
 
         return header
+
+    def _check_side_exists(self, side: Side) -> Tuple[bool, str]:
+        """Used by method plot_one_side. Raise ValueError if the side given
+        is invalid"""
+        if side not in {Side.PORT, Side.STARBOARD}:
+            err = f'Unknown side {side}. Please specify {Side.PORT} or {Side.STARBOARD}.'
+            return False, err
+
+        request_left_failed = side == Side.PORT and not self.header.left
+        request_right_failed = side == Side.STARBOARD and not self.header.right
+
+        if request_left_failed or request_right_failed:
+            err = f'The DVSFile {self.filename} does not contain {side}'
+            return False, err
+        return True, ''
+
+    def _get_ping_array_from_side(self,
+                                  side: Side,
+                                  start_idx: int = 0,
+                                  end_idx: int = None) -> np.ndarray:
+        """Returns a numpy array of pings between (start_idx, end_idx) for
+        the specified side"""
+        if end_idx is None:
+            end_idx = len(self.sss_pings)
+        print(f'start_idx = {start_idx}, end_idx = {end_idx}')
+
+        side_pings = []
+        for idx, ping in enumerate(self.sss_pings):
+            if idx < start_idx or idx >= end_idx:
+                continue
+            for ping_part in ping:
+                if ping_part.side == side:
+                    side_pings.append(ping_part.ping)
+        return np.array(side_pings)
+
+    def plot_one_side(self,
+                      side: Side,
+                      start_idx: int = 0,
+                      end_idx: int = None) -> np.ndarray:
+        """Plot sss pings between (start_idx, end_idx) from the requested side
+        if exists."""
+        success, err = self._check_side_exists(side)
+        if not success:
+            raise ValueError(err)
+        side_pings = self._get_ping_array_from_side(side, start_idx, end_idx)
+
+        plt.figure()
+        plt.imshow(side_pings)
+        plt.title(f'SSS pings from {side} of {self.filename}')
+        return side_pings
+
+    def plot(self, start_idx: int = 0, end_idx: int = None) -> np.ndarray:
+        """Plot all sss pings in the DVSFile"""
+        if self.header.right and not self.header.left:
+            return self.plot_one_side(side=Side.STARBOARD,
+                                      start_idx=start_idx,
+                                      end_idx=end_idx)
+        if self.header.left and not self.header.right:
+            return self.plot_one_side(side=Side.PORT,
+                                      start_idx=start_idx,
+                                      end_idx=end_idx)
+        left_pings = self._get_ping_array_from_side(side=Side.PORT,
+                                                    start_idx=start_idx,
+                                                    end_idx=end_idx)
+        right_pings = self._get_ping_array_from_side(side=Side.STARBOARD,
+                                                     start_idx=start_idx,
+                                                     end_idx=end_idx)
+        sss_image = np.concatenate((np.flip(left_pings, axis=1), right_pings),
+                                   axis=1)
+        plt.figure()
+        plt.imshow(sss_image)
+        plt.title(f'SSS pings from {self.filename}')
+        return sss_image
