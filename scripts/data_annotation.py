aee76824d970080a25e6e960faa249c68cf4bde8
Li Ling <liling@kth.se>
2021-05-19 12:15:16 +0200

    Add skeleton for data annotation

---
+++ scripts/data_annotation.py
@@ -0,0 +1,13 @@
+from dvs_file_reader import SSSPing
+
+
+def rope_annotation(ping: SSSPing):
+    pass
+
+
+def buoy_annotation(ping: SSSPing):
+    pass
+
+
+def nadir_annotation(ping: SSSPing):
+    pass
