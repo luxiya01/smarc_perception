0e1e0aaca62d39e2dd2075f8c41f5ec9a1853da2
Li Ling <liling@kth.se>
2021-05-26 20:09:34 +0200

    Add window sliding function for tentative segmentation

--- scripts/data_annotation.py
+++ scripts/data_annotation.py
@@ -1,13 +1,22 @@
+import numpy as np
+import ruptures as rpt
 from dvs_file_reader import SSSPing
+from typing import List
 
 
-def rope_annotation(ping: SSSPing):
-    pass
+def window_sliding_segmentation(ping: SSSPing,
+                                start_idx: int = 20,
+                                end_idx: int = None,
+                                width: int = 20,
+                                model: str = 'l2',
+                                n_bkps: int = 3) -> List[int]:
+    """Use window sliding method to segment the input SSSPing
+    into (n_bkps + 1) segments. Return the suggested break points."""
+    if not end_idx:
+        end_idx = len(ping.ping)
 
-
-def buoy_annotation(ping: SSSPing):
-    pass
-
-
-def nadir_annotation(ping: SSSPing):
-    pass
+    signal = np.array(ping.ping)
+    algo = rpt.Window(width=width, model=model).fit(signal[start_idx:end_idx])
+    bkps = algo.predict(n_bkps=n_bkps)
+    bkps = [bkps[i] + start_idx for i in range(len(bkps))]
+    return bkps
