e2748db4203a4df27c3ef149aa936f3279f6b461
Li Ling <liling@kth.se>
2021-05-29 17:13:03 +0200

    Add tentative annotation for objects

--- scripts/data_annotation.py
+++ scripts/data_annotation.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
 from enum import Enum
+import json
 import os
 from typing import Dict, List, Tuple
 from matplotlib import pyplot as plt
+import shutil
 import ruptures as rpt
 from dvs_file_reader import SSSPing, DVSFile, Side
 
@@ -21,23 +23,119 @@ class BoundingBox:
     end_idx: int
 
 
-def plot_nadir_annotations(data_dir: str, annotation_dir: str) -> None:
-    dvsfiles = [x for x in os.listdir(data_dir) if x.split('.')[-1] == 'dvs']
+def annotate_objects(data_dir: str, annotation_dir: str,
+                     object_range_json: str):
+    with open(object_range_json, 'r') as f:
+        object_range = json.load(f)
 
-    for filename in dvsfiles:
+    for filename, object_range_dict in object_range.items():
+        print(f'Annotating file {filename}...')
+
         dvsfile = DVSFile(os.path.join(data_dir, filename))
-        nadir = {Side.PORT: {}, Side.STARBOARD: {}}
-        for side, nadir_dict in nadir.items():
-            annotation_file = os.path.join(annotation_dir,
-                                           f'{filename}.{side}.annotation')
-            with open(annotation_file, 'r') as f:
-                for i, line in enumerate(f):
-                    nadir_dict[i] = [int(x) for x in line.split(' ')][-1]
-            _, ax = dvsfile.plot_one_side(side)
-            ax.scatter(x=list(nadir_dict.values()),
-                       y=list(nadir_dict.keys()),
-                       s=2,
-                       c='y')
+        if "Side.PORT" in object_range_dict.keys():
+            object_range_dict[Side.PORT] = object_range_dict["Side.PORT"]
+        if "Side.STARBOARD" in object_range_dict.keys():
+            object_range_dict[
+                Side.STARBOARD] = object_range_dict["Side.STARBOARD"]
+
+        for side, pings in dvsfile.sss_pings.items():
+            nadir_annotation_file = os.path.join(
+                annotation_dir, f'{filename}.{side}.annotation')
+            object_annotation_file = os.path.join(
+                annotation_dir, f'{filename}.{side}.objects.annotation')
+
+            # copy annotation if no need to check for objects
+            if side not in object_range_dict.keys():
+                shutil.copy(nadir_annotation_file, object_annotation_file)
+                continue
+
+            # get indices to annotate for objects
+            object_indices = []
+            for object_range in object_range_dict[side]:
+                object_indices.extend(
+                    list(range(object_range[0], object_range[1])))
+
+            # load nadir annotations
+            nadirs = []
+            with open(nadir_annotation_file, 'r') as f:
+                for line in f:
+                    _, start_idx, end_idx = [
+                        int(x) for x in line.split(' ')[:3]
+                    ]
+                    nadirs.append(BoundingBox(start_idx, end_idx))
+
+            # construct new annotations (nadir + objects) and write to file
+            object_annotation = open(object_annotation_file, 'w')
+            for idx, ping in enumerate(pings):
+                ping_annotation = {}
+                ping_annotation[ObjectID.NADIR] = nadirs[idx]
+
+                if idx in object_indices:
+                    rope = annotate_rope(ping, nadirs[idx])
+                    ping_annotation[ObjectID.ROPE] = rope
+
+                object_annotation.write(
+                    _format_annotation_for_ping(ping_annotation))
+            object_annotation.close()
+
+
+def plot_annotations(data_dir: str, annotation_dir: str) -> None:
+    dvsfiles = sorted([
+        os.path.join(data_dir, x) for x in os.listdir(data_dir)
+        if x.split('.')[-1] == 'dvs'
+    ])
+
+    for dvsfile in dvsfiles:
+        print(dvsfile)
+        plot_annotation_for_file(dvsfile, annotation_dir)
+
+
+def plot_annotation_for_file(dvs_filepath: str, annotation_dir: str) -> None:
+    dvs_filename = os.path.split(dvs_filepath)[-1]
+    dvsfile = DVSFile(dvs_filepath)
+
+    colors = {
+        ObjectID.NADIR.value: 'y',
+        ObjectID.ROPE.value: 'r',
+        ObjectID.BUOY.value: 'k'
+    }
+
+    annotations = {
+        Side.PORT: {
+            ObjectID.NADIR.value: {},
+            ObjectID.BUOY.value: {},
+            ObjectID.ROPE.value: {}
+        },
+        Side.STARBOARD: {
+            ObjectID.NADIR.value: {},
+            ObjectID.BUOY.value: {},
+            ObjectID.ROPE.value: {}
+        }
+    }
+
+    for side, side_annotation in annotations.items():
+        annotation_file = os.path.join(
+            annotation_dir, f'{dvs_filename}.{side}.objects.annotation')
+        with open(annotation_file, 'r') as f:
+            for i, line in enumerate(f):
+                values = [int(x) for x in line.split(' ')]
+                if len(values) >= 3:
+                    side_annotation[values[0]][i] = BoundingBox(
+                        values[1], values[2])
+                if len(values) >= 6:
+                    side_annotation[values[3]][i] = BoundingBox(
+                        values[4], values[5])
+                if len(values) >= 9:
+                    side_annotation[values[6]][i] = BoundingBox(
+                        values[7], values[8])
+
+        _, ax = dvsfile.plot_one_side(side)
+        for k, v in side_annotation.items():
+            y = list(v.keys())
+            x1 = [x.start_idx for x in v.values()]
+            x2 = [x.end_idx for x in v.values()]
+            ax.scatter(x=x1, y=y, s=2, c=colors[k])
+            ax.scatter(x=x2, y=y, s=2, c=colors[k])
     plt.show()
 
 
@@ -108,6 +206,7 @@ def annotate_buoy(ping: SSSPing, nadir: BoundingBox) -> BoundingBox:
     """Given the tentative nadir_annotation, provide tentative buoy
     annotation by segmenting the nadir region. Return None if no
     buoy detected."""
+    min_buoy_width = 10
     buoy_width = 15
     bkps = window_sliding_segmentation(ping=ping,
                                        start_idx=40,
@@ -116,7 +215,7 @@ def annotate_buoy(ping: SSSPing, nadir: BoundingBox) -> BoundingBox:
                                        n_bkps=2)
 
     # Check whether the segmentation is likely to be a buoy
-    if bkps[1] - bkps[0] > buoy_width * 2:
+    if bkps[1] - bkps[0] > buoy_width * 2 or bkps[1] - bkps[0] < min_buoy_width:
         return None
     return BoundingBox(start_idx=bkps[0], end_idx=bkps[1])
 
