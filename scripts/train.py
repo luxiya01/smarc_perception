475ec200766ab5ffd2fd4b93e04de131a522ec1a
Li Ling <liling@kth.se>
2021-06-02 09:39:22 +0200

    Add accuracy computation

--- scripts/train.py
+++ scripts/train.py
@@ -11,9 +11,21 @@ from unet import UNet
 from torch.utils.tensorboard import SummaryWriter
 
 
-def accuracy(label, predicted):
-    label_idx = torch.max(label, dim=0)
-    predicted_idx = torch.max(predicted, dim=0)
+#TODO: compute per object class accuracy
+def compute_accuracy(label, predicted):
+    label_value, label_idx = torch.max(label, dim=2)
+    predicted_value, predicted_idx = torch.max(predicted, dim=2)
+
+    valid_labels = (label_value > 0)
+    num_labels = valid_labels.float().sum()
+    num_correct = torch.logical_and(label_idx == predicted_idx,
+                                    valid_labels).float().sum()
+    print(f'num_labels: {num_labels}, num_correct: {num_correct}')
+    print(f'label_idx: {label_idx}')
+    print(f'predicted_idx: {predicted_idx}')
+    print(f'predicted_value: {predicted_value}')
+    print(f'predicted: {predicted[:, :, 10]}')
+    return num_correct / num_labels
 
 
 train_dataset = SSSData(
@@ -22,20 +34,15 @@ train_dataset = SSSData(
     side=Side.PORT,
     transform=transforms.Compose([ToTensor()]))
 train_dataloader = DataLoader(train_dataset,
-                              batch_size=16,
+                              batch_size=2,
                               shuffle=True,
-                              num_workers=2)
-dim_weights = {
-    ObjectID.NADIR.value: .1,
-    ObjectID.ROPE.value: 1,
-    ObjectID.BUOY.value: 1
-}
+                              num_workers=1)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(f'device = {device}')
 
 writer = SummaryWriter()
-net = UNet(n_classes=3).to(device)
+net = UNet(n_classes=1).to(device)
 optimizer = optim.RMSprop(net.parameters())
 loss_function = nn.MSELoss()
 
@@ -57,9 +64,11 @@ for epoch in range(num_epochs):
         losses.append(loss.item())
         running_loss += loss.item()
         if i % 50 == 0:
+            accuracy = compute_accuracy(label, predicted)
             running_loss /= 50
             print(
-                f'[epoch {epoch}, batch {i}] average loss: {running_loss:.5f}')
+                f'[epoch {epoch}, batch {i}] average loss: {running_loss:.5f}, current accuracy: {accuracy}'
+            )
             running_loss = 0
     average_loss = sum(losses) / len(losses)
     writer.add_scalar('loss-epoch/train', average_loss, epoch)
